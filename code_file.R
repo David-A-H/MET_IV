360efe5fb509fda0f15b3c5c73d60a8c334e2248
David-A-H <davidhollenstein17@gmail.com>
2025-05-20 17:13:40 +0200

    Implement first half of the pre-processing pipeline

--- code_file.R
+++ code_file.R
@@ -1,9 +1,14 @@
 
+# MAIN TASK:
+# Your task is to build a model to predict if a respondent reports any plans to emigrate (Variable emig).
+
 ### COMMENCE SETUP ##########################################################################################
 
 # Load Packages
 ## Import Packages here:
 library(tidyverse)
+library(tidymodels)
+library(caret)
 
 # Clear Global Environment
 rm(list=ls())
@@ -14,13 +19,48 @@ data <-  train
 rm(train)
 
 
-### COMMENCE PRE-PROCESSING ######################################################################################
+
+### COMMENCE PRE-PROCESSING #################################################################################
+
+# Locate NA values in the dataset
+na_count <- sapply(data, function(y) sum(length(which(is.na(y)))))
+na_count[na_count > 0]
+
+## NA Values seem to be in Columns "age" and "sex"
+# Removing NA values (gets rid of 336 observations)
+data <- na.omit(data)
+
+#################################################################################
+
+# Overall Summary
+summary(data)
+## NOTE: Main issue is that variables have very unintuitive names. Main objective of this step: Rename variables and check for major class imbalance.
+
+# Current names of variables
+names(data)
 
 
+  
+#################################################################################
+
+# Split Data into Train and Test and Stratify for emig
+set.seed(123)
+train_index <- createDataPartition(data$emig, p = .8, list = FALSE)
+train <- data[train_index, ]
+test <- data[-train_index, ]
+
+# Check for dependent Variable proportion in train and test
+prop.table(table(train$emig))
+prop.table(table(test$emig))
+
+# Create backup data file
+backup <-  data
+
+# Clear global environment except for "train", "test", "backup" and "eval"
+rm(list=ls()[!ls() %in% c("train", "test", "backup", "eval")])
 
 
-
-### COMMENCE PREDICTION ######################################################################################
+### COMMENCE PREDICTION #####################################################################################
 # NOTE: BEFORE EVERY PREDICTION MODEL, RUN set.seed(123) to reset the RNG.
 
 
@@ -28,7 +68,7 @@ rm(train)
 
 
 
-### COMMENCE MODEL EXTRACTION ######################################################################################
+### COMMENCE MODEL COMPARISON AND EXTRACTION #################################################################
 
 
 
