24ffd5163cb51d2d393e3a170b7302130d8e7cfe
David-A-H <davidhollenstein17@gmail.com>
2025-05-21 09:36:30 +0200

    Implement rest of pre-processing pipeline, including tool for variable renaming

--- code_file.R
+++ code_file.R
@@ -35,12 +35,32 @@ data <- na.omit(data)
 # Overall Summary
 summary(data)
 ## NOTE: Main issue is that variables have very unintuitive names. Main objective of this step: Rename variables and check for major class imbalance.
+# 
+# # Current names of variables
+# names(data)
+# 
+# #Import rename-frame 
+# var_rename <- read_csv("var_rename.csv")
+# 
+# # Make new_label the new column names in data
+# data <- data %>%
+#   rename_with(~ var_rename$new_label[match(., var_rename$original_label)], everything())
 
-# Current names of variables
-names(data)
-
-
+# NOTE: Renaming code works. However, the recoding file "var_rename.csv" is not entirely correct because it was created by AI. If worth the effort, maybe re-check. Otherwise proceed without renaming variables
   
+#################################################################################
+
+# Check for class imbalance
+table(data$emig)
+## NOTE: Class imbalance is present. 1 = emigration, 0 = no emigration
+
+# Check for class imbalance in other variables
+hist(data$age)
+hist(as.numeric(data$income), breaks = 17)
+hist(as.numeric(data$edu_combined))
+
+# NOTE: Might want to think about rescaling some values like age. Slight skewness towards younger respondents
+
 #################################################################################
 
 # Split Data into Train and Test and Stratify for emig
@@ -55,6 +75,9 @@ prop.table(table(test$emig))
 
 # Create backup data file
 backup <-  data
+
+# # Load evaluation data (Used for evaluating the model at the very end)
+# eval <- load("final_project/AB4x_eval_mock.Rdata")
 
 # Clear global environment except for "train", "test", "backup" and "eval"
 rm(list=ls()[!ls() %in% c("train", "test", "backup", "eval")])
